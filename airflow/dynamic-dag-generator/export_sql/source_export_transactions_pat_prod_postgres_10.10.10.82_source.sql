f72b8e9bf38d2260ed9ec3c96af6de2b4a3c1ab9
Bhuvanesh <47310439+BhuviTheDataGuy@users.noreply.github.com>
2021-04-06 12:27:04 +0530

    Update source_export_transactions_pat_prod_postgres_10.10.10.82_source.sql

--- airflow/dynamic-dag-generator/export_sql/source_export_transactions_pat_prod_postgres_10.10.10.82_source.sql
+++ airflow/dynamic-dag-generator/export_sql/source_export_transactions_pat_prod_postgres_10.10.10.82_source.sql
@@ -7,7 +7,7 @@ select
 	tbl1.time_modified,
 	case
 		when tbl1.jcol::json ->>'renewal' = '' then 0
-		else (tbl1.jcopl::json ->>'renewal')::int
+		else (tbl1.jcol::json ->>'renewal')::int
 	end renewal,
 	tbl1.update_timestamp
 from
